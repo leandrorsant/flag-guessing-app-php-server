ea414f94d22bb819d1b8cc4f8a5654d302f4d792
Leandro <ramosdossantos.leandro@gmail.com>
2023-11-02 17:42:42 -0300

    First version

--- database/database.php
+++ database/database.php
@@ -23,22 +23,95 @@ class DBConnection {
     }
 
     public function findByName($name){
-        $d = $this->collection->findOne(['name' => $name]);
-        if($d != null){
-            $user  =new User(($d->_id->__toString()), $d->name, $d->password, $d->highscore, $d->session_id ?? "");
-            return $user;
+        if($name){
+            $d = $this->collection->findOne(['name' => $name]);
+            if($d != null){
+                $user  =new User(($d->_id->__toString()), $d->name, $d->password, $d->highscore, $d->session_id ?? "");
+                return $user;
+            }
         }
         return [];
     }
 
     public function findById($id){
-        //var_dump(new MongoDB\BSON\ObjectId());
-        $d = $this->collection->find(['_id'=> (new \MongoDB\BSON\ObjectID($id)) ]);
-        if($d != null){
-            $user  =new User(($d->_id->__toString()), $d->name, $d->password, $d->highscore, $d->session_id ?? "");
-            return $user;
+        if($id){
+            try{
+                $d = $this->collection->findOne(['_id'=> (new \MongoDB\BSON\ObjectID($id)) ]);
+                if($d != null){
+                    $user  = new User(($d->_id->__toString()), $d->name, $d->password, $d->highscore, $d->session_id ?? "");
+                    return $user;
+                }
+            }catch(Exception $e){
+
+            }
         }
         return [];
+    }
+
+    public function findBySessionId($session_id){
+        if($session_id){
+            try{
+                $d = $this->collection->findOne(['session_id'=> $session_id ]);
+                if($d != null){
+                    $user = new User(($d->_id->__toString()), $d->name, $d->password, $d->highscore, $d->session_id ?? "");
+                    return $user;
+                }
+            }catch(Exception $e){
+                return [];
+            }
+        }
+        return [];
+    }
+
+    public function addUser($name, $password, $highscore){
+        if($this->findByName($name)){
+            return null;
+        }else{
+            try{
+                $insertOneResult = $this->collection->insertOne([
+                    'name' => $name,
+                    'password' => $password,
+                    'highscore' => $highscore
+                ]);
+                
+                $user = $this->findByName($name);
+                return $user;
+
+            }catch (Exception $e){
+                return null;
+            }
+            return null;
+        }
+    }
+
+    public function userLogin($session_id, $name, $password){
+        $user = $this->findBySessionId($session_id);
+        
+        if(!$user){
+            $user = $this->findByName($name);
+            return $user;
+            if($user){
+                if($user->password == $password){
+                    $new_session_id = new \MongoDB\BSON\ObjectId();
+                    $new_session_id->__toString();
+                    $this->collection->updateOne(['name' => $name], ['$set' => ['session_id' => $new_session_id] ]);
+                }else{
+                   return null;
+                }
+            }
+        }
+        return $user;
+    }
+
+    public function updateHighscore($user_id, $password, $highscore){
+        $user = $this->findById($user_id);
+        if($user){
+            if($user->password == $password){
+                $this->collection->updateOne(['_id' => new \MongoDB\BSON\ObjectId($user_id)], ['$set' => ['highscore' => $highscore]]);
+                return $user;
+            }     
+        }
+        return null;
     }
 
     public function getConnection(){
--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      header('Access-Control-Allow-Origin: *');
+      header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+      header('Access-Control-Allow-Headers: token, Content-Type');
+      header('Access-Control-Max-Age: 1728000');
+      header('Content-Length: 0');
+      header('Content-Type: text/plain');
+      die();
+  }
+
+  header('Access-Control-Allow-Origin: *');
+   
+
    header('Content-Type: application/json');
    require_once __DIR__ . '/database/database.php';
    
-   $request = $_SERVER['REQUEST_URI'];
+   $request = rtrim($_SERVER['REQUEST_URI'], '/');
    
    $request_parts = explode('/', $request);
   
    $db = new DBConnection();
 
 if($_SERVER["REQUEST_METHOD"] === 'GET'){
+   
    if($request_parts[1] == 'users'){
 
       // Get all users (/users)
@@ -20,9 +34,9 @@ if($_SERVER["REQUEST_METHOD"] === 'GET'){
       // Get user by id (/users/:id)
       else if(count($request_parts) == 3){
          $user_id = $request_parts[2];
-         $user = $db->findByName($user_id);
+         $user = $db->findById($user_id);
          //echo json_encode($user); 
-         echo json_encode(new MongoDB\BSON\ObjectId());
+         echo json_encode($user);
       }
 
       // Get user by name (/users/name/:name)
@@ -30,9 +44,108 @@ if($_SERVER["REQUEST_METHOD"] === 'GET'){
          $user_name = $request_parts[3]; 
          $user = $db->findByName($user_name);
          http_response_code(201);
-         echo json_encode($user);
+         echo json_encode([$user]);
       }
 
+   }
+}else if($_SERVER["REQUEST_METHOD"] === 'POST'){
+   //echo json_encode($request_parts);
+   
+   if($request_parts[1] == 'users'){
+      $request_body = json_decode(file_get_contents('php://input'), true);
+      
+      //create user (/user)
+      error_log(json_encode($request_parts), 0); 
+      if(count($request_parts) == 2){
+         //$users = $db->getAllUsers();
+         $name = $request_body['name'];
+         $password = $request_body['password'];
+         $highscore = 0;
+         if(array_key_exists('highscore', $request_body)){
+            $highscore = $request_body['highscore'];
+         }
+        
+         $newUser = $db->addUser($name, $password, $highscore);
+         if($newUser){
+            http_response_code(201);
+                           
+            echo json_encode([$newUser]);
+         }else{
+            http_response_code(400);
+            echo json_encode(['message' => "Bad request"]);
+         }
+
+      }
+
+      //login by session
+      else if($request_parts[2] == 'session' && count($request_parts) == 3){
+         if(array_key_exists('session_id', $request_body)){
+            $session_id = $request_body['session_id'];
+            $user = $db->findBySessionId($session_id);
+            if($user){
+               http_response_code(201);
+               echo json_encode([$user]);
+            }else{
+               http_response_code(404);
+               echo json_encode(['message' => "Invalid session_id"]);
+            }
+            
+         }else{
+            http_response_code(404);
+            echo json_encode(['message' => "Invalid session_id"]);
+         }
+         
+         //echo json_encode("create new session"); 
+      }else if($request_parts[2] == 'login' && count($request_parts) == 3){
+        // echo json_encode($request_body);
+         $session_id = null;
+         $name = null;
+         $password = null
+         ;
+         if(array_key_exists('session_id',$request_body)){
+            $session_id = $request_body['session_id'];
+         }
+         if(array_key_exists('name',$request_body)){
+            $name = $request_body['name'];
+         }
+         if(array_key_exists('password',$request_body)){
+            $password = $request_body['password'];
+         }
+         
+         $user = $db->userLogin($session_id, $name, $password);
+         echo json_encode([$user]);
+         
+      }
+
+   }
+}else if($_SERVER["REQUEST_METHOD"] === 'PATCH'){
+   if($request_parts[1] == 'users'){
+      $request_body = json_decode(file_get_contents('php://input'), true);
+      
+     
+      //update highscore
+      if(count($request_parts) == 3){
+         $user_id = $request_parts[2];
+         $password = null;
+         $highscore = null;
+
+         if(array_key_exists('password', $request_body)){
+            $password = $request_body['password'];
+         }
+         if(array_key_exists('highscore', $request_body)){
+            $highscore = $request_body['highscore'];
+         }
+
+         $user = $db->updateHighscore($user_id, $password, $highscore);
+         if($user){
+            echo json_encode($user);
+         }else{
+            http_response_code(404);
+            echo json_encode(['message' => 'Incorrect password']);
+         }
+
+         
+      }
    }
 }
 
